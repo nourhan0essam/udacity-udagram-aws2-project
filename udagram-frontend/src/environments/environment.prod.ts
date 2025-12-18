0a3921b070899f80580cbdeb90c2b842a9b9ff0f
C005276 <neelessawy@ejada.com>
2025-12-18 15:20:41 +0200

    second-commit

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: "Udagram",
-  apiHost: "http://localhost:8080/api/v0",
+  apiHost: "http://Demo-app-env.eba-mhqt99eq.us-east-1.elasticbeanstalk.com/api/v0",
 };
 
 /*
